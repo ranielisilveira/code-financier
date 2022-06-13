c7f679bbf200175a22ca040cb848e188c3bd315c
Ranieli <ranielisilveira@hotmail.com>
2022-06-13 20:11:39 -0300

    adjust extends

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,7 +12,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/admin.css')}}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
